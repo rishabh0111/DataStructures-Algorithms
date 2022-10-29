514d031239da39aced306575f12260e1c1be887f
RishabhSharma8912 <20bcs3694@cuchd.in>
2022-10-29 13:34:31 +0530

    Modified delete & search function

--- Linked List/Linear_Linked_List.cpp
+++ Linked List/Linear_Linked_List.cpp
@@ -6,77 +6,130 @@ class Node{
     int data;
     Node *next;
 
+    // Constructor
     Node(int data) {
       this->data = data;
       this->next = NULL;
     }
+
+    // Destructor
+    // ~Node() {
+    //   int value = this ->data;
+    //   // memory free
+    //   if (this -> next != NULL) {
+    //     delete next;
+    //     this->next = NULL;
+    //   }
+    //   cout << "Memory is free for node with data " << value << endl;
+    // }
 };
 
 class LinkedList {
   public:
     Node* head;
+    Node* tail;
 
-    void push_front(Node* &head, int d);
-    void push_back(Node* &tail, int d);
-    Node* back();
-    Node* search(int x);
-    Node* erase(int x);
-    void print(Node* &head);
+    void push_front(int d);
+    void push_back(int d);
+    void insert(int position, int d);
+    bool search(int x);
+    void erase(int val);
+    void print();
     bool empty();
 
     LinkedList() { head = NULL; }
 };
 
 // Insert element at Front
-void LinkedList::push_front(Node* &head, int d) {
+void LinkedList::push_front(int d) {
   Node* temp = new Node(d);
   temp -> next = head;
   head = temp;
 }
 
 // Insert element at Back
-void LinkedList::push_back(Node* &tail, int d) {
+void LinkedList::push_back(int d) {
   Node* temp = new Node(d);
-  tail -> next = temp;
+  tail-> next = temp;
   tail = temp;
 }
 
-// Returns last element
-Node *LinkedList::back() {
-  Node* ptr = head;
-  while(ptr->next != NULL) {
-    ptr = ptr->next;
+// Insert element at required position
+void LinkedList::insert(int position, int d) {
+  //Inserting at start
+  if(position == 1) {
+    push_front(d);
+    return;
   }
-  return ptr;
+
+  Node *temp = head;
+  int cnt = 1;
+  while (cnt < position - 1)
+  {
+    temp = temp -> next;
+    cnt++;
+  }
+
+  //Inserting at last
+  if(temp -> next == NULL) {
+    push_back(d);
+    return;
+  }
+
+  Node *nodeToInsert = new Node(d);
+  nodeToInsert -> next = temp -> next;
+  temp -> next = nodeToInsert;
 }
 
-// Search for element
-Node* LinkedList::search(int x) {
-  Node* ptr = head;
-  while(ptr->next != NULL && ptr->data != x) {
-    ptr = ptr->next;
+// Search for element //Iterative Approach:
+bool LinkedList::search(int x) {
+  Node* current = head;
+  while(current != NULL) {
+    if(current->data == x)
+      return true;
+    current = current->next;
   }
-  return ptr;
+  return false;
 }
 
-// Delete an element
-Node* LinkedList::erase(int x) {
-  Node* n = search(x);
-  Node* ptr = head;
-  if (ptr == n) {
-    ptr->next = n->next;
-    return n;
-  } else {
-    while(ptr->next != n) {
-      ptr = ptr->next;
-    }
-    n->next = ptr->next;
-    return n;
+// Search for element // Recursive Approach:
+// After uncommenting change function definition accordingly
+// bool LinkedList::search(Node* &head,int x) {
+//   if(head == NULL)
+//     return false;
+//   if(head->data == x)
+//     return true;
+//   return search(head->next,x);
+// }
+
+// Delete element in list:
+void LinkedList::erase(int val) {
+
+  if(head == NULL) {
+    return;
   }
+  // Deleting Start Node
+  if(head->data == val) {
+    Node* to_delete = head;
+    head = head->next;
+    delete to_delete;
+    return;
+  }
+  
+  // Deleting any Middle or Last node
+  Node* temp = head;
+  while(temp->next->data != val) {
+    temp = temp->next;
+  }
+
+  Node* to_delete = temp->next;
+  temp->next = temp->next->next;
+
+  delete to_delete;
 }
 
 // Print List
-void LinkedList::print(Node* &head) {
+void LinkedList::print() {
   Node* temp = head;
 
   while(temp != NULL) {
@@ -99,22 +152,36 @@ int main() {
   LinkedList L;
   Node* node1 = new Node(10);
 
-  Node* head = node1;
-  Node* tail = node1;
+  L.head = node1;
+  L.tail = node1;
 
-  L.print(head);
-  L.push_front(head,12);
-  L.push_front(head,15);
-  L.print(head);
+  L.print();
+  L.push_front(12);
+  L.push_front(15);
+  L.print();
 
-  L.push_back(tail,21);
-  L.push_back(tail,24);
-  L.print(head);
+  L.push_back(21);
+  L.push_back(24);
+  L.print();
 
-  if(L.empty())
-    cout << "List is empty";
-  else
-    cout << "List is not empty";
+  L.insert(6,44);
+  L.print();
+
+  cout << "head: " << L.head->data << endl;
+  cout << "tail: " << L.tail->data << endl;
+  
+  L.search(21) ? cout << "Yes": cout << "No";
+  cout << endl;
+  L.search(33) ? cout << "Yes": cout << "No";
+  cout << endl;
+
+  L.empty() ? cout << "List is empty": cout << "List is not empty";
+  cout << endl;
+
+  L.erase(15); // deleting first node
+  L.erase(21); // deleting middle node
+  L.erase(44); // deleting last node
+  L.print();
 
   return 0;
 }
